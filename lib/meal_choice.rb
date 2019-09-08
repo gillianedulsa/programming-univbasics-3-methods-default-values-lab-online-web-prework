267323707247ea7327695a77a31ca5e74ca1bb03
Maria Guillian A. Edulsa <edulsag03@gmail.com>
2019-09-08 00:47:22 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,1 +1,8 @@
-# Your code here
+Expect(meal_choice_1("brussel sprouts","broccolini")).to eq("A meal with brussel sprouts and broccolini")
+Expect(meal_choice("brussel sprouts","broccolini","steak")).to eq("A meal with brussel sprouts, broccolini and steak")
+def meal_choice_1
+  puts "What a nutritious meal!" 
+def meal_choice_2
+  puts "A plate of meat with brussel sprouts and broccolini."
+  expect($stdout).to receive(:puts).with("What a nutritious meal!")
+  expect($stdout).to receive(:puts).with("A plate of meat with brussel sprouts and broccolini
